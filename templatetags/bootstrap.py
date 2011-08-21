331d6066ef64d55d0f314cf7523f3b9ed4a257ed
tzangms <tzangms@gmail.com>
2011-08-21 09:07:02 -0700

    initial commit

---
+++ templatetags/bootstrap.py
@@ -0,0 +1,15 @@
+from django.template import Context
+from django.template.loader import get_template
+from django import template
+
+register = template.Library()
+
+@register.filter
+def bootstrap(form):
+    template = get_template("bootstrap_form/form.html")
+    context = Context({'form': form})
+    return template.render(context)
+
+@register.filter
+def is_checkbox(field):
+    return field.field.widget.__class__.__name__.lower() == "checkboxinput"
