3b22c9e421e352fee9e5d77464baed73f748b9ca
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2013-07-28 23:14:05 +0200

    First try for bootstrap 3.0

--- bootstrapform/templatetags/bootstrap.py
+++ bootstrapform/templatetags/bootstrap.py
@@ -4,23 +4,40 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def bootstrap(element):
     element_type = element.__class__.__name__.lower()
 
     if element_type == 'boundfield':
+        classes = element.field.widget.attrs.get('class', '')
+        classes += ' form-control'
+        element.field.widget.attrs['class'] = classes
+
         template = get_template("bootstrapform/field.html")
         context = Context({'field': element})
     else:
         has_management = getattr(element, 'management_form', None)
         if has_management:
+            for form in element.forms:
+                for field in form.visible_fields():
+                    classes = field.field.widget.attrs.get('class', '')
+                    classes += ' form-control'
+                    field.field.widget.attrs['class'] = classes
+
             template = get_template("bootstrapform/formset.html")
             context = Context({'formset': element})
         else:
+            for field in element.visible_fields():
+                classes = field.field.widget.attrs.get('class', '')
+                classes += ' form-control'
+                field.field.widget.attrs['class'] = classes
+
             template = get_template("bootstrapform/form.html")
             context = Context({'form': element})
-        
+
     return template.render(context)
+
 
 @register.filter
 def is_checkbox(field):
