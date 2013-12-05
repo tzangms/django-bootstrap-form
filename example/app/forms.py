23229b2f9ddb80fed10a97ae68146e86d341c2b0
tzangms <tzangms@gmail.com>
2013-12-05 22:47:27 +0800

    Update example project
    Add
    
    * multiple choice
    * multiple checkbox

--- example/app/forms.py
+++ example/app/forms.py
@@ -10,6 +10,8 @@ class ExampleForm(forms.Form):
     char_field = forms.CharField()
     choice_field = forms.ChoiceField(choices=CHOICES)
     radio_choice = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    multiple_choice = forms.MultipleChoiceField(choices=CHOICES)
+    multiple_checkbox = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple)
     file_fied = forms.FileField()
     password_field = forms.CharField(widget=forms.PasswordInput)
     textarea = forms.CharField(widget=forms.Textarea)
