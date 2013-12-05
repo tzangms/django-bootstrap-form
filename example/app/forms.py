435b5c691aa459b2482056f88794757eeb96b4bd
tzangms <tzangms@gmail.com>
2013-12-05 22:39:01 +0800

    Update example with bootstrap 3

--- example/app/forms.py
+++ example/app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
+CHOICES = (
+    (0, 'Zero'), 
+    (1, 'One'), 
+    (2, 'Two'),
+)
+
 class ExampleForm(forms.Form):
-    title = forms.CharField()
-    choices = forms.ChoiceField(choices=((0, 'Zero'), (1, 'One'), (2, 'Two')), widget=forms.RadioSelect)
-    password = forms.CharField(widget=forms.PasswordInput)
+    char_field = forms.CharField()
+    choice_field = forms.ChoiceField(choices=CHOICES)
+    radio_choice = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    file_fied = forms.FileField()
+    password_field = forms.CharField(widget=forms.PasswordInput)
     textarea = forms.CharField(widget=forms.Textarea)
+    boolean_field = forms.BooleanField()
