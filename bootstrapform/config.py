a959bb34db999bd9b789dcb4ee171026cdf69b83
Stéphane Raimbault <stephane.raimbault@gmail.com>
2014-04-28 16:09:25 +0200

    Cleanup bootstrap_horizontal computing
    * rewrite split and class building
    * new default value for label column count
    * comments

--- bootstrapform/config.py
+++ bootstrapform/config.py
@@ -1,4 +1,7 @@
 from django.conf import settings
 
 
+BOOTSTRAP_DEFAULT_VALUE_COLUMN_COUNT = getattr(settings, 'BOOTSTRAP_DEFAULT_VALUE_COLUMN_COUNT', 2)
+
 BOOTSTRAP_COLUMN_COUNT = getattr(settings, 'BOOTSTRAP_COLUMN_COUNT', 12)
+BOOTSTRAP_LABEL_COLS = getattr(settings, 'BOOTSTRAP_LABEL_COLS', 'col-sm-2 col-lg-2')
