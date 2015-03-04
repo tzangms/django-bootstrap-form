d2193303d84131bff91ceab71e2be506c054d7ca
Nik Nyby <nnyby@columbia.edu>
2015-03-04 13:34:28 -0500

    Use field.id_for_label instead of field.auto_id
    This fixes a bug when specifying a custom id for a field and rendering
    it with the `|bootstrap` filter.
    
    When using `field.auto_id` in the label's `for` attribute, this is
    always set to the default value instead of the custom `id`.
    
    Also fix a typo in the tests: file_fied -> file_field

--- bootstrapform/tests.py
+++ bootstrapform/tests.py
@@ -28,7 +28,7 @@ class ExampleForm(forms.Form):
     radio_choice = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
     multiple_choice = forms.MultipleChoiceField(choices=CHOICES)
     multiple_checkbox = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple)
-    file_fied = forms.FileField()
+    file_field = forms.FileField()
     password_field = forms.CharField(widget=forms.PasswordInput)
     textarea = forms.CharField(widget=forms.Textarea)
     boolean_field = forms.BooleanField()
