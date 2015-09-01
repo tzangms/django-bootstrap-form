bb26fd61c16c004391f1bc8983d5bd95a73710c5
Peter Bengtsson <mail@peterbe.com>
2015-09-01 16:05:53 -0700

    forkismus maximus

--- example/app/forms.py
+++ example/app/forms.py
@@ -1,18 +1,21 @@
 from django import forms
 
 CHOICES = (
-    (0, 'Zero'), 
-    (1, 'One'), 
+    (0, 'Zero'),
+    (1, 'One'),
     (2, 'Two'),
 )
 
 class ExampleForm(forms.Form):
     char_field = forms.CharField()
+    optional_char_field = forms.CharField(required=False)
     choice_field = forms.ChoiceField(choices=CHOICES)
     radio_choice = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
     multiple_choice = forms.MultipleChoiceField(choices=CHOICES)
-    multiple_checkbox = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple)
+    multiple_checkbox = forms.MultipleChoiceField(
+        choices=CHOICES, widget=forms.CheckboxSelectMultiple)
     file_fied = forms.FileField()
     password_field = forms.CharField(widget=forms.PasswordInput)
     textarea = forms.CharField(widget=forms.Textarea)
     boolean_field = forms.BooleanField()
+    optional_boolean_field = forms.BooleanField(required=False)
