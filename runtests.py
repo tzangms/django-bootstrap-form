1fc7e3af58f429a6792413789bf321097ea3900c
lampslave <lampslave@gmail.com>
2020-05-01 13:29:26 +0300

    use settings template from django 2.2 - fix runtests.py again

--- runtests.py
+++ runtests.py
@@ -31,7 +31,10 @@ settings.configure(
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
                     'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
                 ],
             },
         },
